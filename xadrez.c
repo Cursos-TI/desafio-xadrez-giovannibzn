c15e536607b075c4c9e0ef4389e18cb0a681e8a3
giovannibzn <giovannibiazon@gmail.com>
2025-02-11 01:05:44 +0000

    finalizado usando os 3 tipos de loops de repetição

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,34 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int bispo = 1;
+    int torre = 1;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    while(bispo <= 5){
+        printf("Bispo para Cima\n");
+        printf("e para Direita %d\n", bispo);
+        bispo++;
+    }
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\n");
+
+    do{
+        printf("Torre para direita %d\n", torre);
+        torre++;
+    }while(torre <= 5);
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("\n");
+
+    for(int rainha = 1; rainha <=8; rainha++){
+        printf("Rainha esquerda %d\n", rainha);
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
